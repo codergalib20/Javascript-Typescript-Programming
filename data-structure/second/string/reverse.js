2e5c8750ffe409b7793226a04af984e4f16e908d
codergalib20 <coderboygalif@gmail.com>
2022-12-27 20:19:06 +0600

    string reverse

---
+++ data-structure/second/string/reverse.js
@@ -0,0 +1,30 @@
+// ---- Directions
+// Give a string, return a new string with the reverse
+// order of characters
+// --- Examples
+// reverse('apple') = 'elppa'
+// reverse('ball') = 'llab'
+
+function reverse(str) {
+  const array = [];
+  const strLength = str.length - 1;
+  for (let i = strLength; i >= 0; i--) {
+    array.push(str[i]);
+  }
+  return array.join('');
+}
+
+// console.log(reverse("apple"));
+// console.log(reverse("Galib"));
+// console.log(reverse("Asadullah"));
+
+
+function reverse2 (str){
+    const array = str.split('');
+    array.reverse();
+    return array.join('')
+}
+
+// console.log(reverse2('apple'))
+// console.log(reverse2('galib'))
+// console.log(reverse2('asadullah'))
