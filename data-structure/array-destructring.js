9a37a0e4a6668cf45caa03b0e6de822454140537
codergalib20 <coderboygalif@gmail.com>
2023-01-01 10:56:03 +0600

    commited after learning array destructring switching variable array value

--- data-structure/array-destructring.js
+++ data-structure/array-destructring.js
@@ -15,11 +15,14 @@ const [x, y, z] = arr;
 console.log(x, y, z);
 console.log(arr);
 
-
 let [main, secondary] = resturant.categories;
 console.log(main, secondary);
 
-const temp = main;
-main = secondary;
-secondary = temp;
-console.log(main, secondary);
+// ------------- Switching Variable
+// const temp = main;
+// main = secondary;
+// secondary = temp;
+// console.log(main, secondary);
+
+[main, secondary] = [secondary, main];
+console.log(main, secondary)
