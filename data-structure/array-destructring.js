4367f7bd2660f67bc6935f4613743db9b82fc708
codergalib20 <coderboygalif@gmail.com>
2023-01-01 10:47:50 +0600

    commite after learning basic arrray destructuring

---
+++ data-structure/array-destructring.js
@@ -0,0 +1,15 @@
+const resturant = {
+  name: "Classico Italiano",
+  location: "Via Angelo Tavanti 23, Firenze, Italy",
+  categories: ["Italian", "Pizzaria", "Vigetarian"],
+  startMenu: ["Foccaccia", "Bruschetta", "Garlic", "Bread", "Caprese sa;ad"],
+  mainMenu: ["Pizza", "Pasta", "Risatta"],
+};
+
+const arr = [2, 3, 4];
+const a = arr[0];
+const b = arr[1];
+const c = arr[2];
+
+const [x, y, z] = arr;
+console.log(x, y, z);
