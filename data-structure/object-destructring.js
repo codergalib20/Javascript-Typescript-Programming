72703cbf0c93f758f09d6a737bf923884ed453c6
codergalib20 <coderboygalif@gmail.com>
2023-01-01 11:12:56 +0600

    commited after learning object destructring default value

---
+++ data-structure/object-destructring.js
@@ -0,0 +1,28 @@
+const resturant = {
+  name: "Classico Italiano",
+  location: "Via Angelo Tavanti 23, Firenze, Italy",
+  categories: ["Italian", "Pizzeria", "Vegetarian", "Organic"],
+  starterMenu: ["Focaccia", "Bruschetta", "Garlic", "Bread", "Caprese Salad"],
+  mainMenu: ["Pizza", "Pasta", "Risotto"],
+  openingHours: {
+    thu: {
+      open: 12,
+      close: 22,
+    },
+    fri: {
+      open: 11,
+      close: 23,
+    },
+    sat: {
+      open: 0,
+      close: 24,
+    },
+  },
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.starterMenu[mainIndex]];
+  },
+};
+
+const {name, openingHours, categories} = resturant;
+
+console.log(name, openingHours, categories);
