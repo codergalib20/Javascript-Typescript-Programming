debbf641adb2cf5b7ed04d5a70b1f06d6ecd0950
codergalib20 <coderboygalif@gmail.com>
2023-01-02 22:30:04 +0600

    learning and practiching linkedlist data structure in javascript

--- data-structure/linkedlist.js
+++ data-structure/linkedlist.js
@@ -10,4 +10,16 @@ class LinkedList {
   constructor() {
     this.head = null;
   }
-}
+  // Insert First
+  insertFirst(data) {
+    const newData = (this.head = new Node(data, this.head));
+    return this.head;
+  }
+};
+
+
+const myLinkedList = new LinkedList();
+console.log(myLinkedList.insertFirst({ name: "Galib", school: "Nilphamari" }));
+console.log(myLinkedList.insertFirst({ name: "Galib", school: "Nilphamari" }));
+console.log(myLinkedList.insertFirst({ name: "Galib", school: "Nilphamari" }));
+
