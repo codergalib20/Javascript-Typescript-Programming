b204b51370f4d36fbb1506e8fa3ad5ceb0b8a96a
codergalib20 <coderboygalif@gmail.com>
2023-01-02 22:14:52 +0600

    learning and practiching linkedlist data structure in javascript

---
+++ data-structure/linkedlist.js
@@ -0,0 +1,13 @@
+// Node ------
+class Node {
+  constructor(data, next = null) {
+    this.data = data;
+    this.next = next;
+  }
+}
+// List
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+}
