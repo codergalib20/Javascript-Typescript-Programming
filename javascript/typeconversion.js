e75f6b234aeb1f30445420c32eba4843a08218e4
codergalib20 <coderboygalif@gmail.com>
2023-01-12 22:04:41 +0600

    commited

---
+++ javascript/typeconversion.js
@@ -0,0 +1,11 @@
+var str = "1000";
+var n = 10;
+
+console.log(str * n);
+console.log(str + n);
+
+console.log(Number(str));
+console.log(String(n));
+
+console.log(Number.parseInt(str));
+console.log(Number.parseFloat(str));
