3332446a942a533733e8eab8944b1301466f528e
codergalib20 <coderboygalif@gmail.com>
2023-01-20 00:23:22 +0600

    Learn to ES6 spread operator

--- [...]spret-operator.js
+++ [...]spret-operator.js
@@ -1,6 +1,15 @@
-const arr = [1, 2, 3];
-const badNewArray = [1, 2, arr[0], arr[1], arr[2]];
+// const arr = [1, 2, 3];
+// const badNewArray = [1, 2, arr[0], arr[1], arr[2]];
 
-console.log(badNewArray);
-const newArr = [1, 2, ...arr];
-console.log(arr);
+// console.log(badNewArray);
+// const newArr = [1, 2, ...arr];
+// console.log(arr);
+
+const restaurant = {
+  menu: [1, 2, 3, 4, 4, 5],
+  members: ["Rifat", "Forhad", "Galib", "Mazidul"],
+};
+
+const newArr = ["Badhon", ...restaurant.members];
+
+console.log(newArr);
