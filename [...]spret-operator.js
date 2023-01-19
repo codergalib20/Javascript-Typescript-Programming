b4e02e51c29e334c64ad949ef8244a5b2ea70e9b
codergalib20 <coderboygalif@gmail.com>
2023-01-20 00:20:51 +0600

    Learn to ES6 spread operator in javascript

--- [...]spret-operator.js
+++ [...]spret-operator.js
@@ -1,1 +1,6 @@
-const arr = [1, 2, 3, 4, 5, 6];
+const arr = [1, 2, 3];
+const badNewArray = [1, 2, arr[0], arr[1], arr[2]];
+
+console.log(badNewArray);
+const newArr = [1, 2, ...arr];
+console.log(arr);
