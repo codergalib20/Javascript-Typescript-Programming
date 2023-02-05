b00f2df368d1559f8b705f7a285e67a63ec4e87f
codergalib20 <coderboygalif@gmail.com>
2023-02-05 20:01:03 +0600

    learning advance javascript type

---
+++ advance javascript zero to mastery/type.js
@@ -0,0 +1,6 @@
+// console.log(typeof 5);
+console.log(typeof true);
+console.log(typeof 'To be or not to be');
+console.log(typeof undefined);
+console.log(typeof null);
+console.log(typeof Symbol('just me'))
