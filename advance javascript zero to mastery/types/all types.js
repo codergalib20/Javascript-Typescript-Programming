948be081b900a0e1beeeb0081190ee3c0f06c2a4
codergalib20 <coderboygalif@gmail.com>
2023-02-05 20:23:09 +0600

    check type

--- advance javascript zero to mastery/types/all types.js
+++ advance javascript zero to mastery/types/all types.js
@@ -1,9 +1,12 @@
 // console.log(typeof 5);
-console.log(typeof true);
-console.log(typeof 'To be or not to be');
-console.log(typeof undefined);
-console.log(typeof null);
-console.log(typeof Symbol('just me'));
-console.log(typeof {});
-console.log(typeof Object());
-console.log(typeof []); 
+// console.log(typeof true);
+// console.log(typeof 'To be or not to be');
+// console.log(typeof undefined);
+// console.log(typeof null);
+// console.log(typeof Symbol('just me'));
+// console.log(typeof {});
+// console.log(typeof Object());
+console.log(typeof [4]); 
+
+
+// console.log(this);
---
+++ advance javascript zero to mastery/types/typecheck.js
@@ -0,0 +1,7 @@
+console.log(
+  Array.isArray([
+    /* 2,3,4,5,5 */
+  ])
+);
+
+console.log(Object.create({}))
