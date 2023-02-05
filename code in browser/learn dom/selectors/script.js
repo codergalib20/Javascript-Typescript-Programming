f0f252489854a5d36c4a0794b1fd169ae5935e13
codergalib20 <coderboygalif@gmail.com>
2023-02-05 16:50:10 +0600

    learning javscript dom event listener basic to advance

--- code in browser/learn dom/selectors/script.js
+++ code in browser/learn dom/selectors/script.js
@@ -1,17 +1,21 @@
-console.log(document.querySelector('.message'));
-console.log(document.querySelector('#message'));
-console.log(document.querySelector('h2'));
+console.log(document.querySelector(".message"));
+console.log(document.querySelector("#message"));
+console.log(document.querySelector("h2"));
 
-console.log(document.getElementById('message'));
-console.log(document.getElementsByClassName('message'));
-console.log(document.getElementsByTagName('h2'));
+console.log(document.getElementById("message"));
+console.log(document.getElementsByClassName("message"));
+console.log(document.getElementsByTagName("h2"));
 
-document.getElementById('message').innerHTML ='<span>This is get element by id</span>';
-document.querySelector('h2').textContent = 'Welcome to text content'
+document.getElementById("message").innerHTML =
+  "<span>This is get element by id</span>";
+document.querySelector("h2").textContent = "Welcome to text content";
 
-document.querySelector('.message').innerText = 'Welcome to inner Text class message'
-console.log('Input field > ', document.querySelector('input').value = '012345678');
-
+document.querySelector(".message").innerText =
+  "Welcome to inner Text class message";
+console.log(
+  "Input field > ",
+  (document.querySelector("input").value = "012345678")
+);
 
 // document.querySelector('button').addEventListener('click', () => {
 //     document.querySelector('input').value = '';
@@ -19,6 +23,14 @@ console.log('Input field > ', document.querySelector('input').value = '012345678
 // })
 
 // document.querySelector('button').addEventListener('click', function () {
-    // console.log(this);
+// console.log(this);
 // })
 
+const eventFunc = function () {
+  console.log(this);
+  this.innerText = "My name is button";
+  this.disabled = true;
+  this.type = 'submit';
+};
+
+document.querySelector("button").addEventListener("click", eventFunc);
