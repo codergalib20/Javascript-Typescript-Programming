9e8568e88df0858ac35f43e64de497a7785ab610
codergalib20 <coderboygalif@gmail.com>
2023-02-05 16:30:33 +0600

    learning dom text inner text text content inner html

--- code in browser/learn dom/selectors/script.js
+++ code in browser/learn dom/selectors/script.js
@@ -7,3 +7,6 @@ console.log(document.getElementsByClassName('message'));
 console.log(document.getElementsByTagName('h2'));
 
 document.getElementById('message').innerHTML ='<span>This is get element by id</span>';
+document.querySelector('h2').textContent = 'Welcome to text content'
+
+document.querySelector('.message').innerText = 'Welcome to inner Text class message'
