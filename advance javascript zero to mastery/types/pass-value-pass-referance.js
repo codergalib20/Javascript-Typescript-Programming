5071a51e544bb297aa9305da45c7b7b4518050b7
codergalib20 <coderboygalif@gmail.com>
2023-02-05 20:41:28 +0600

    commited pass value reference

--- advance javascript zero to mastery/types/pass-value-pass-referance.js
+++ advance javascript zero to mastery/types/pass-value-pass-referance.js
@@ -1,5 +1,6 @@
 var a = 2;
 var b = a;
+b++;
 console.log(b);
 
 // let obj1 = {name : "Galib", password: '222'};
@@ -10,9 +11,24 @@ console.log(b);
 // console.log(obj1);
 // console.log(obj2);
 
-var c = [1, 2, 3, 4, 5];
-var d = [].concat(c);
+// var c = [1, 2, 3, 4, 5];
+// var d = [].concat(c);
+//
+// d.push(18434343);
+//
+// console.log(d);
 
-d.push(18434343);
-
-console.log(d);
+let obj = {
+  a: "a",
+  b: "b",
+  c: {
+    deep: "Try and copy me",
+  },
+};
+let clone = Object.assign({}, obj);
+let clone2 = { ...obj };
+// obj.c = 3;
+obj.c.deep = 'hahahaha'
+console.log(obj);
+console.log(clone);
+console.log(clone2);
