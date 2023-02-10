ba4e9303cd307b787f343074410dec3e55db8dd2
codergalib20 <coderboygalif@gmail.com>
2023-02-10 15:10:30 +0600

    make my custom own array in javascript add get and push method method

--- data-structure-javascript/zeroToMastery/array/custom-array.js
+++ data-structure-javascript/zeroToMastery/array/custom-array.js
@@ -4,11 +4,22 @@ class MyArray {
     this.data = {};
   }
 
+  // Get method in javascript
   get(index) {
     return this.data[index];
+  }
+
+  // Push method in Javascript
+  push(item) {
+    this.data[this.length] = item;
+    this.length++;
+    return this.length;
   }
 }
 
 const newArray = new MyArray();
-
-console.log(newArray)
+newArray.push('Hi!')
+newArray.push('How!')
+newArray.push('Are ')
+newArray.push('You')
+console.log(newArray);
