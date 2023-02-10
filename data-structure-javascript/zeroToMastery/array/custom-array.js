525f5420d87b1812058fabae68497207de848eb2
codergalib20 <coderboygalif@gmail.com>
2023-02-10 15:12:32 +0600

    added pop method in ym custom javscript array

--- data-structure-javascript/zeroToMastery/array/custom-array.js
+++ data-structure-javascript/zeroToMastery/array/custom-array.js
@@ -15,11 +15,20 @@ class MyArray {
     this.length++;
     return this.length;
   }
+
+  // Added Pop Method in Javascript
+  pop() {
+    const item = this.data[this.length - 1];
+    delete this.data[this.length - 1];
+    this.length--;
+    return item;
+  }
 }
 
 const newArray = new MyArray();
-newArray.push('Hi!')
-newArray.push('How!')
-newArray.push('Are ')
-newArray.push('You')
+newArray.push("Hi!");
+newArray.push("How!");
+newArray.push("Are ");
+newArray.push("You");
+newArray.pop();
 console.log(newArray);
