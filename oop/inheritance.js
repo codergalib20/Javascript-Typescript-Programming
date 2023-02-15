86de809a040df52a71e04d36ffeab06c4018c305
codergalib20 <coderboygalif@gmail.com>
2023-02-16 01:08:57 +0600

    start learning class extends super

--- oop/inheritance.js
+++ oop/inheritance.js
@@ -1,18 +1,28 @@
-class Elf {
-    constructor(name, weapon) {
-        this.name = name;
-        this.weapon = weapon;
-    }
- 
-    attack () {
-        return 'attack with ' + this.weapon;
-    }
+class Character {
+  constructor(name, weapon) {
+    this.name = name;
+    this.weapon = weapon;
+  }
+
+  attack() {
+    return "attack with " + this.weapon;
+  }
 }
 
-const fiona = new Elf('Fiona', 'ninja stars');
-const ogre = {...fiona};
-console.log(ogre);
+class Elf extends Character {
+  constructor(name, weapon, type) {
+    super(name, weapon);
+    this.type = type;
+  }
+}
 
-console.log(ogre.__proto__);
-console.log(fiona.__proto__);
-console.log(fiona === ogre);
+// const fiona = new Elf("Fiona", "ninja stars");
+// const ogre = { ...fiona };
+// console.log(ogre);
+
+// console.log(ogre.__proto__);
+// console.log(fiona.__proto__);
+// console.log(fiona === ogre);
+
+const dolby = new Elf("Dolby", "cloth", "house");
+console.log(dolby);
