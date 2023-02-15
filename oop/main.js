f08a842e3addbf30210b600b7f6afa69cdea5911
codergalib20 <coderboygalif@gmail.com>
2023-02-15 16:28:50 +0600

    create dymanic code replace of object using function createElf

--- oop/main.js
+++ oop/main.js
@@ -1,9 +1,27 @@
-const elf = {
-  name: "Orwell",
-  weapon: "bow",
-  attack() {
-    return "attack with " + elf.weapon;
-  },
-};
+// const elf = {
+//   name: "Orwell",
+//   weapon: "bow",
+//   attack() {
+//     return "attack with " + elf.weapon;
+//   },
+// };
 
-console.log(elf)
+// console.log(elf)
+
+// factory functions
+
+function createElf(name, weapon) {
+  return {
+    name: name,
+    weapon: weapon,
+    attack() {
+      return "attack with " + weapon;
+    },
+  };
+}
+
+const peter = createElf("Peter", "stones");
+const sam = createElf("Sam", "fire");
+
+console.log(peter.attack());
+console.log(sam.attack());
