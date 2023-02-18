5acb67e9061257972ad51c47f533bfc2f5dd0ea3
codergalib20 <coderboygalif@gmail.com>
2023-02-18 15:53:36 +0600

    hash table custom

--- data-structure-javascript/zeroToMastery/hash table/hash table first practice.js
+++ data-structure-javascript/zeroToMastery/hash table/hash table first practice.js
@@ -1,11 +1,28 @@
-const user = {
-  name: "Galib",
-  age: 17,
-  magic : true,
-  scream : function () {
-    console.log('Ahhha');
+// const user = {
+//   name: "Galib",
+//   age: 17,
+//   magic : true,
+//   scream : function () {
+//     console.log('Ahhha');
+//   }
+// };
+// user.age;
+// user.name;
+// user.scream();
+
+class HashTable {
+  constructor(size) {
+    this.data = new Array(size);
   }
-};
-user.age;
-user.name;
-user.scream();
+  _hash(key) {
+    let hash = 0;
+    for (let i = 0; i < key.length; i++) {
+      hash = (hash + key.charCodeAt(i) * i) % this.data.length;
+    }
+    return hash;
+  }
+}
+
+const myHashTable = new HashTable(50);
+
+console.log(myHashTable._hash("grapes"));
