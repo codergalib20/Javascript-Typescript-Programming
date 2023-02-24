e7bb1fe735d7de37d645b84286737e32f72fc7cf
codergalib20 <coderboygalif@gmail.com>
2023-02-24 16:09:30 +0600

    array

--- javascript/functinon.js
+++ javascript/functinon.js
@@ -1,11 +1,19 @@
-const calcAvarage = (a, b, c) => (a + b + c) / 3;
+// const calcAvarage = (a, b, c) => (a + b + c) / 3;
 
-const checkWinner = function (avgDolhins, avgKoalas) {
-  if (avgKoalas >= 2 * avgKoalas) {
-    console.log(`Dolphins win (${avgDolhins} vs. ${avgKoalas})`);
-  } else if (avgKoalas >= 2 * avgDolhins) {
-    console.log(`Koalas win (${avgKoalas} vs. ${avgDolhins})`);
-  } else {
-    console.log("No team wins");
-  }
-};
+// const checkWinner = function (avgDolhins, avgKoalas) {
+//   if (avgKoalas >= 2 * avgKoalas) {
+//     console.log(`Dolphins win (${avgDolhins} vs. ${avgKoalas})`);
+//   } else if (avgKoalas >= 2 * avgDolhins) {
+//     console.log(`Koalas win (${avgKoalas} vs. ${avgDolhins})`);
+//   } else {
+//     console.log("No team wins");
+//   }
+// };
+
+const firend1 = "Michael";
+const firend2 = "Steven";
+const friend3 = "Peter";
+
+const friends = ["Micheal", "Steven", "Peter"];
+
+console.log(friends);
