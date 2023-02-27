87179b5f4329c8be428f86f8c5cc7905b1ca0c29
codergalib20 <coderboygalif@gmail.com>
2023-02-27 21:21:48 +0600

    learning object oriented programming

--- javascript/prototype.js
+++ javascript/prototype.js
@@ -10,3 +10,28 @@
 
 // console.log(foo.prototype);
 // console.log(foo.prototype = {});
+
+// function Gadget(name, color) {
+//   this.name = name;
+//   this.color = color;
+//   this.whatAreYou = function () {
+//     return "I am a " + this.color + " " + this.name;
+//   };
+// }
+
+// Gadget.prototype.price = 100;
+// Gadget.prototype.rating = 3;
+// Gadget.prototype.getInfo = function () {
+//   return "Rating : " + this.rating + " price : " + this.price;
+// };
+
+// // Gadget.prototype = {
+// //   price: 100,
+// //   rating: 4,
+// // };
+
+
+// const newtoy = new Gadget('Webcam', 'black');
+// console.log(newtoy)
+// console.log(newtoy.getInfo())
+
---
+++ javascript/tempCodeRunnerFile.js
@@ -0,0 +1,4 @@
+t.prototype = {
+//   price: 100,
+//   rating: 4,
+// };
