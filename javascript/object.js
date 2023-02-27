6ab0a16035084ac167f741331ed001099baf114e
codergalib20 <coderboygalif@gmail.com>
2023-02-27 20:22:08 +0600

    learning object oriented programming

--- javascript/object.js
+++ javascript/object.js
@@ -7,3 +7,25 @@
 // };
 
 // console.log(hero.say());
+
+// let hero = {
+//     name: "Rafaelo",
+//     sayName : function () {
+//         return this.name;
+//     }
+// }
+
+// function Hero(name) {
+//   this.occupation = "Ninja";
+//   this.name = name;
+//   this.whoAreYou = function () {
+//     return "I'm " + this.name + " and I'm a " + this.occupation;
+//   };
+// }
+
+// const hero1 = new Hero("Galib");
+// console.log(hero1.whoAreYou());
+// const hero2 = new Hero("Asadullah");
+// console.log(hero2.whoAreYou());
+// const hero3 = new Hero("Hi!");
+// console.log(hero3.whoAreYou());
