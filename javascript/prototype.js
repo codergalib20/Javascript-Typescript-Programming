9c02523a942103f0aa68ecd7093384d1c368faca
codergalib20 <coderboygalif@gmail.com>
2023-02-27 21:16:07 +0600

    learning object oriented programming

---
+++ javascript/prototype.js
@@ -0,0 +1,12 @@
+// function foo (a,b,c) {
+//     return a * b * c;
+// }
+
+// console.log(foo.length);
+
+// console.log(foo.constructor())
+
+// console.log(typeof foo.prototype);
+
+// console.log(foo.prototype);
+// console.log(foo.prototype = {});
