4d6760859ae60da09adbc50b424a5502e42d57f0
codergalib20 <coderboygalif@gmail.com>
2023-02-28 14:55:02 +0600

    learning typescript function parameters

---
+++ typescript/complete-roadmap/04 - function-types/function-parameter-annotations.ts
@@ -0,0 +1,7 @@
+function square(num: number): number {
+  return num * num;
+}
+
+square(3);
+// square("asad"); // -> "Error"
+// square(true); // -> "Error"
--- typescript/complete-roadmap/04 - function-types/function-parameter.ts
+++
@@ -1,8 +0,0 @@
-function square(num) {
-  return num * num;
-}
-
-square(3);
-square("asad");
-square(true);
- 
