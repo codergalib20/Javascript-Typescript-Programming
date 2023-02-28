fe9e557141b40c102a270d0977695d2b4498a6a9
codergalib20 <coderboygalif@gmail.com>
2023-02-28 15:12:14 +0600

    function return types annotaton

---
+++ typescript/complete-roadmap/04 - function-types/function-return-types-annotation.ts
@@ -0,0 +1,9 @@
+const add = (x: number, y: number): number => {
+  return x + y;
+};
+const introduction = (name: string, age: number): string => {
+  return `This is ${name}, I'm ${age} now`;
+};
+const isregular = (type: boolean) : boolean => {
+    return type;
+}
