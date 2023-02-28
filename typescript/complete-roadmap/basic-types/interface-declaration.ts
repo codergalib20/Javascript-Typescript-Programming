270029cbdc93c852a0717174b143546cc17fca58
codergalib20 <coderboygalif@gmail.com>
2023-02-28 14:20:18 +0600

    learning typescript

---
+++ typescript/complete-roadmap/basic-types/interface-declaration.ts
@@ -0,0 +1,9 @@
+// Type Interface
+
+let tvShow = "Olive Kitteridge";
+tvShow = "The Other Two"; // Changed
+// tvShow = false; -> "Error"
+
+let isFunny = false;
+isFunny = true; // Changed
+// isFunny = "true" -> "Error";
