63e8b08eeafa25601da59475a06b18906e30ace6
codergalib20 <coderboygalif@gmail.com>
2023-02-28 14:56:52 +0600

    learning function string parameters

--- typescript/complete-roadmap/04 - function-types/function-parameter-annotations.ts
+++ typescript/complete-roadmap/04 - function-types/function-parameter-annotations.ts
@@ -5,3 +5,12 @@ function square(num: number): number {
 square(3);
 // square("asad"); // -> "Error"
 // square(true); // -> "Error"
+
+function greet(person: string) {
+  return `Hi! there ${person}`;
+}
+
+greet("Galib");
+// greet(17); // -> "Error"
+// greet(true); // -> "Error"
+// greet({name : "Galib"}) // -> "Error"
