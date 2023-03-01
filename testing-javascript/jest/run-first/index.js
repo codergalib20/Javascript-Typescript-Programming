8d52e3f3c7c2eb075203ac9a55e6ebf41f9c2471
codergalib20 <coderboygalif@gmail.com>
2023-03-01 20:59:13 +0600

    first test is success

--- testing-javascript/jest/run-first/index.js
+++ testing-javascript/jest/run-first/index.js
@@ -1,4 +1,16 @@
 function sum(a, b) {
   return a + b;
 }
-module.exports = sum;
+
+function sumOfArray(array) {
+  let sum = 0;
+
+  for (let i = 0; i < array.length; i++) {
+    sum += array[i];
+  }
+  console.log(sum);
+}
+
+sumOfArray();
+
+module.exports = { sum, sumOfArray };
--- testing-javascript/jest/run-first/index.test.js
+++ testing-javascript/jest/run-first/index.test.js
@@ -1,2 +1,10 @@
-const sum = require("./index");
-console.log(sum());
+const { sum } = require("./index");
+
+test("add  1 + 2 to equal 3", () => {
+  expect(sum(1, 2)).toBe(3);
+});
+
+// Test Looping array
+test("summanation of number array 1 to 10", () => {
+  expect(sum([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]).toBe(44));
+});
