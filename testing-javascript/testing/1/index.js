4a1760649e37b7dadbe1d0673dd8a3202b5f77ae
codergalib20 <coderboygalif@gmail.com>
2023-03-04 20:20:05 +0600

    learning testing in javascript

---
+++ testing-javascript/testing/1/index.js
@@ -0,0 +1,17 @@
+const sum = (a, b) => a + b;
+const substruction = (a, b) => a - b;
+
+let result = sum(3, 7);
+let expected = 10;
+
+if (result !== expected) {
+  throw new Error(`${result} is not equal to ${expected}`);
+}
+
+result = substruction(7, 3);
+
+expected = 4;
+
+if (result !== expected) {
+  throw new Error(`${result} is not equal to ${expected}`);
+}
