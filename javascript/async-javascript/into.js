6a3bb85f8e0c1d0c5071559de20a7405db0bc004
codergalib20 <coderboygalif@gmail.com>
2023-03-04 22:58:06 +0600

    commited

--- javascript/async-javascript/into.js
+++ javascript/async-javascript/into.js
@@ -1,11 +1,12 @@
 setTimeout(() => {
-  console.log("Hello first time out");
-}, 0);
-
-setTimeout(() => {
-  console.log("Hello second time out");
-}, 1000);
-
-Promise.resolve("hi").then((data) => console.log("2", data));
-
-console.log("3", "is a crow");
+    console.log("Hello first time out");
+  }, 0);
+  
+  setTimeout(() => {
+    console.log("Hello second time out");
+  }, 1000);
+  
+  Promise.resolve("hi").then((data) => console.log("2", data));
+  
+  console.log("3", "is a crow");
+  
