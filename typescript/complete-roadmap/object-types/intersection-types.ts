6d7ba594cfb880c3b7fa8d59888bc6cb114be656
codergalib20 <coderboygalif@gmail.com>
2023-03-07 20:30:40 +0600

    learning typescript intersection type in object

--- typescript/complete-roadmap/object-types/intersection-types.ts
+++ typescript/complete-roadmap/object-types/intersection-types.ts
@@ -12,3 +12,22 @@ const happyFace: ColorCircle = {
   redius: 4,
   color: "navy",
 };
+
+type Cat = {
+  numLives: number;
+};
+
+type Dog = {
+  breed: string;
+};
+
+type CatDog = Cat &
+  Dog & {
+    age: number;
+  };
+
+const christy: CatDog = {
+  numLives: 7,
+  breed: "Husky",
+  age: 9,
+};
