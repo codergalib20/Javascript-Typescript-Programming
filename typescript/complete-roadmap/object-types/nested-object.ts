3a14d6ca9738c81c53ac60e390ee3a762d4e8172
codergalib20 <coderboygalif@gmail.com>
2023-03-07 20:19:19 +0600

    learning typescript

---
+++ typescript/complete-roadmap/object-types/nested-object.ts
@@ -0,0 +1,23 @@
+type Song = {
+  title: string;
+  artist: string;
+  numStreams: number;
+  credits: { producer: string; writer: string };
+};
+function calculatePayout(song: Song): number {
+  return song.numStreams * 0.0033;
+}
+
+function printSong(song: Song): void {
+  console.log(`${song.title} - ${song.artist}`);
+}
+
+calculatePayout({
+  title: "Unchaied Melody",
+  artist: "Righteous Brothers",
+  numStreams: 343434,
+  credits: {
+    producer: "Phil spector",
+    writer: "Alex North",
+  },
+});
