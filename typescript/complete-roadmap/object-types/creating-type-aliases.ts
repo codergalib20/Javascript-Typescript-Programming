3ec3f4a5e042efe382f26246966c95936e3ad0b5
codergalib20 <coderboygalif@gmail.com>
2023-03-07 20:11:46 +0600

    learning typescript

--- typescript/complete-roadmap/object-types/creating-type-aliases.ts
+++ typescript/complete-roadmap/object-types/creating-type-aliases.ts
@@ -2,3 +2,17 @@ type MyNum = number;
 
 const age: MyNum = 17;
 const birth: MyNum = 17;
+
+type StudentModel = {
+  name: string;
+  age: number;
+  active: boolean;
+  id: number;
+};
+
+const student1: StudentModel = {
+  name: "Galib",
+  age: 17.5,
+  active: true,
+  id: 54545,
+};
