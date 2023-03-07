a505583b5369a817723a6fce4491f9a71d38813d
codergalib20 <coderboygalif@gmail.com>
2023-03-07 19:39:50 +0600

    learning typescript

--- typescript/complete-roadmap/object-types/object-type.ts
+++ typescript/complete-roadmap/object-types/object-type.ts
@@ -1,6 +1,12 @@
-const information = {
-    name : "Galib",
-    breed: "Bangladeshi",
-    age: 7.5,
-    
+// const information = {
+//     name : "Galib",
+//     breed: "Bangladeshi",
+//     age: 7.5,
+
+// }
+
+function printName(person: { first_name: string; last_name: string }): void {
+    console.log(`My name is ${person.first_name} ${person.last_name}`);    
 }
+
+console.log(printName({"Galib", "goin"}));
