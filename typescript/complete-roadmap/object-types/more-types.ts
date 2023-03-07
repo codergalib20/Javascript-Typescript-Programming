d35d36f8c91549c69c3b57da064dd3d8594a860d
codergalib20 <coderboygalif@gmail.com>
2023-03-07 19:44:37 +0600

    learning typescript

--- typescript/complete-roadmap/object-types/more-types.ts
+++ typescript/complete-roadmap/object-types/more-types.ts
@@ -2,3 +2,9 @@ let coordinate: {
   x: number;
   y: number;
 } = { x: 100, y: 200 };
+
+function randomCordinate(): { x: number; y: number } {
+  return { x: Math.random(), y: Math.random() };
+}
+
+randomCordinate();
