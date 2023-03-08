d2c6c8846162e31d5cda439d90466a02dc01c80c
codergalib20 <coderboygalif@gmail.com>
2023-03-08 20:54:04 +0600

    learning typescript problem solving

---
+++ problem-solving/same-array.ts
@@ -0,0 +1,16 @@
+function same(arr1: number[], arr2: number[]) {
+  if (arr1.length !== arr2.length) {
+    throw new Error("Array langth are not same");
+  }
+
+  for (let i = 0; i < arr1.length; i++) {
+    let currentIndex = arr2.indexOf(arr1[i] ** 2);
+    if (currentIndex === -1) {
+      return false;
+    }
+    arr2.splice(currentIndex, 1);
+    return true;
+  }
+}
+
+console.log(same([2, 3, 4, 52], [3, 4, 5, 5]));
