b071a8c6b454f825fe0a19e1fe5114a3a20e8b6f
codergalib20 <coderboygalif@gmail.com>
2023-03-08 18:40:44 +0600

    commited

--- problem-solving/chareter to object.ts
+++ problem-solving/chareter to object.ts
@@ -1,5 +1,13 @@
-let name: string = "Asadullah Hil Galib";
+const arr: number[] = [1, 2, 3, 4, 4, 5, 6, 6, 7, 8, 8, 6, 4, 5, 4, 5, 6];
 
-const arr = [Set(...name)];
+const uniqueArray = () => {
+  let temp: number[] = [];
+  for (let i: number = 0; i < arr.length; i++) {
+    if (!temp.includes(arr[i])) {
+      temp.push(arr[i]);
+    }
+  }
+  return temp;
+};
 
-console.log(arr);
+console.log(uniqueArray());
