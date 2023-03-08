74ab958a65645f3f7a81a3922ca45a8a9227d711
codergalib20 <coderboygalif@gmail.com>
2023-03-08 20:35:52 +0600

    commited

--- problem-solving/Character count.ts
+++ problem-solving/Character count.ts
@@ -1,10 +1,20 @@
-function charCount(str: string) {
-  let charList: object = {};
+// function charCount(str: string) {
+//   let charList: object = {};
 
-  for (let i = 0; i < str.length; i++) {
-    let char = str[i];
-    console.log(charList[char]);
-  }
-}
+//   for (let i = 0; i < str.length; i++) {
+//     let char = str[i];
+//     if (char.match(/[a-zA-Z]/)) {
+//       if (charList[char] > 0) {
+//         charList[char]++;
+//       } else {
+//         charList[char] = 1;
+//       }
+//     }
+//   }
+// }
 
-charCount("Asadullah Hil Galib");
+// charCount("Asadullah Hil Galib");
+
+
+let charList: any = {};
+console.log(charList['a'])
