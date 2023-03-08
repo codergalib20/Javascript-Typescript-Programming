93157990e10261f755cc1013211a7e380f512601
codergalib20 <coderboygalif@gmail.com>
2023-03-08 19:56:41 +0600

    learning problem solving with typescript (find the higher and lower number from the array of number)

---
+++ problem-solving/find-higher-lower-number.ts
@@ -0,0 +1,23 @@
+const arr: number[] = [23, 3, 4, 5,1, 2, 323, 4, 34, 2, 33, 2];
+
+const findHigherAndLowerNumber = (arr: number[]) => {
+  // defind higher and lower variable
+  let higher: number = arr[0];
+  let lower: number = arr[0];
+
+  // iterate the whole array
+  for (let i = 0; i < arr.length; i++) {
+    if (higher < arr[i]) {
+      higher = arr[i];
+    } else if (lower > arr[i]) {
+      lower = arr[i];
+    }
+  }
+  // return the numbers
+  return {
+    lower,
+    higher,
+  };
+};
+
+console.log(findHigherAndLowerNumber(arr));
