8c4ac62b36831850b43c85844e64099f67bf2982
codergalib20 <coderboygalif@gmail.com>
2023-03-11 21:41:30 +0600

    commited

--- typescript/complete-roadmap/union-types/literal-types.ts
+++ typescript/complete-roadmap/union-types/literal-types.ts
@@ -1,18 +1,7 @@
-type Point = {
-  lat: number;
-  long: number;
-};
-type Loc = {
-  x: number;
-  y: number;
-};
-
-const coords: (Point | Loc)[] = [];
-
-coords.push({ lat: 3434.34, long: 3434 });
-coords.push({ x: 343, y: 343 });
-
-
 // let zero : 0 = 0;
 
 // zero = 3;
+
+// let hi: "hi" = "Hi!";
+
+// hi = "Hello";
