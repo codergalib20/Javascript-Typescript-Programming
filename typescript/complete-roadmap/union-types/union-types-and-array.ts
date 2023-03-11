0219226efd5a00fd21edba49623224cef7f7c0ce
codergalib20 <coderboygalif@gmail.com>
2023-03-11 21:38:55 +0600

    commited

--- typescript/complete-roadmap/union-types/union-types-and-array.ts
+++ typescript/complete-roadmap/union-types/union-types-and-array.ts
@@ -11,3 +11,17 @@ const nums: (number | string | boolean | object)[] = [
   true,
   { name: "Galib" },
 ];
+
+type Point = {
+  lat: number;
+  long: number;
+};
+type Loc = {
+  x: number;
+  y: number;
+};
+
+const coords: (Point | Loc)[] = [];
+
+coords.push({ lat: 3434.34, long: 3434 });
+coords.push({ x: 343, y: 343 });
