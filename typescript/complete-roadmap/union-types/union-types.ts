0e12e639d731d488c9ace8a7e35adbb1899efbd5
codergalib20 <coderboygalif@gmail.com>
2023-03-11 21:21:43 +0600

    commited

--- typescript/complete-roadmap/union-types/union-types.ts
+++ typescript/complete-roadmap/union-types/union-types.ts
@@ -3,3 +3,20 @@ let age: number | string | boolean = 21;
 age = 32;
 age = "32";
 age = true;
+
+let active: string | boolean | number = true;
+active = 1;
+active = "true";
+active = false;
+
+type Point = {
+  x: number;
+  y: number;
+};
+
+type Loc = {
+  lat: number;
+  long: number;
+};
+
+
