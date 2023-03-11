2327fecbf09cc56b921481dff024f0d0e86303e7
codergalib20 <coderboygalif@gmail.com>
2023-03-11 21:54:05 +0600

    commited

--- typescript/complete-roadmap/union-types/literal-types.ts
+++ typescript/complete-roadmap/union-types/literal-types.ts
@@ -7,21 +7,32 @@
 // hi = "Hello";
 
 // A function with a literal+union type parameter
-const giveAnswer = (answer : "yes" | "no" | "maybe" ) => {
-    return `The answer is ${answer}.`;
-}
+const giveAnswer = (answer: "yes" | "no" | "maybe") => {
+  return `The answer is ${answer}.`;
+};
 
 // CAN provide one of the literals in the union
 giveAnswer("no"); // ✔️ The answer is no.
 
 // CAN't provide anything else
-giveAnswer("oh boy I'm not sure");  // ❌ It will show error
+giveAnswer("oh boy I'm not sure"); // ❌ It will show error
 
+let mood: "Happy" | "Sad" = "Happy";
 
+mood = "ok"; // It will show error
 
-let mood : "Happy" | "Sad" = "Happy";
+mood = "Sad"; // Acceptable
+mood = "sad"; // Not Acceptable
 
-mood = "ok" // It will show error
+type DayOfWeek =
+  | "Monday"
+  | "Tuesday"
+  | "Wednesday"
+  | "Thursday"
+  | "Friday"
+  | "Saturday"
+  | "Sunday";
 
-mood = "Sad" // Acceptable
-mood = "sad" // Not Acceptable
+let today: DayOfWeek = "Friday";
+today = "Monday";
+today = "Sunday";
