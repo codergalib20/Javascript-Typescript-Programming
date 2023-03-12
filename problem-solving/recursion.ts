c1d23b3c55ee62a4320a9074d0f8db13062e27d0
codergalib20 <coderboygalif@gmail.com>
2023-03-12 20:37:15 +0600

    learning problem solving data structure and algorithms in Typescript and Javascript

--- problem-solving/recursion.ts
+++ problem-solving/recursion.ts
@@ -1,15 +1,25 @@
 // Learning Recursion
 
-function countDown(n: number) {
-  // for(let i = n; i > 0; i--) {
-  //     console.log(i);
-  // }
+// function countDown(n: number) {
+//   // for(let i = n; i > 0; i--) {
+//   //     console.log(i);
+//   // }
 
-  console.log(n);
-  n--;
-  if (n > 0) {
-    countDown(n);
+//   console.log(n);
+//   n--;
+//   if (n > 0) {
+//     countDown(n);
+//   }
+// }
+
+// countDown(21);
+
+function sumOfNumber(n: number): number {
+  let sum = 0;
+  for (let i = n; i > 0; i--) {
+    sum += i;
   }
+  return sum;
 }
 
-countDown(21);
+console.log(sumOfNumber(10))
